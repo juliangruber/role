9eea0023c875be7c4a0140a5c0a4ce821bd58fdc
Julian Gruber <julian@juliangruber.com>
2013-10-07 13:41:21 +0200

    added test for main listening

---
+++ test/main-listens.js
@@ -0,0 +1,33 @@
+var spawn = require('child_process').spawn;
+var test = require('tap').test;
+var uppercaser = __dirname + '/../example/uppercaser.js';
+var extend = require('xtend');
+
+test('main listens', function(t) {
+  t.plan(1);
+
+  var upper = spawn('node', [uppercaser], {
+    env: extend(process.env, {
+      CONNECT: '8900',
+      ROLE: 'uppercaser'
+    })
+  });
+  var main = spawn('node', [uppercaser], {
+    env: extend(process.env, {
+      LISTEN: '8900',
+      ROLE: 'main'
+    })
+  });
+
+  upper.stderr.pipe(process.stderr, { end: false });
+  main.stderr.pipe(process.stderr, { end: false });
+
+  main.stdout.on('data', function(d) {
+    t.equal(d.toString(), 'FOO');
+    upper.kill();
+    main.kill();
+  });
+
+  main.stdin.write('foo');
+});
+
